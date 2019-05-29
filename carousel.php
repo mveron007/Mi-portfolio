8939499cede5fe242e6e5a51db2356e92d1caa42
mveron007 <mveron13814@gmail.com>
2019-05-29 20:23:33 -0300

    agregar .gitignore

---
+++ carousel.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="es" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="css/carousel-style.css">
+    <title></title>
+  </head>
+  <body>
+    <div id="carouselExampleFade" class="carousel slide carousel-fade" data-ride="carousel">
+      <div class="carousel-inner">
+        <div class="carousel-item active">
+          <img src="images/coder.jpg" class="d-block w-100" alt="...">
+        </div>
+        <div class="carousel-item">
+          <img src="images/bioengineer-2.jpg" class="d-block w-100" alt="...">
+        </div>
+        <div class="carousel-item">
+          <img src="images/electromecánico.jpg" class="d-block w-100" alt="...">
+
+        </div>
+      </div>
+      <a class="carousel-control-prev" href="#carouselExampleFade" role="button" data-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="sr-only">Previous</span>
+      </a>
+      <a class="carousel-control-next" href="#carouselExampleFade" role="button" data-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="sr-only">Next</span>
+      </a>
+    </div>
+  </body>
+</html>
---
+++ footer.php
@@ -0,0 +1,36 @@
+!DOCTYPE html>
+<html lang="es" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/animate.css">
+    <link rel="stylesheet" href="css/footer.css">
+
+    <title></title>
+  </head>
+  <body>
+    <footer class="d-block w-100 main-footer-contain">
+      <nav class="main-footer-nav">
+        <ul class="redes">
+          <div >
+            <li class="social-link fb"> <a href="#"> <img src="images/fb-icon.png"> </a> </li>
+          </div>
+          <div >
+            <li class="social-link ln"> <a href="https://www.linkedin.com/in/marcos-dami%C3%A1n-ver%C3%B3n-21814b182/"> <img src="images/linkedin.png"> </a> </li>
+          </div>
+          <div >
+            <li class="social-link gh"> <a href="#"> <img src="images/github-logo.png"> </a> </li>
+          </div>
+
+        </ul>
+
+      </nav>
+
+    </footer>
+
+    <script src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
+
+
+  </body>
+</html>
