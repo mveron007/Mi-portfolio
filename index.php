5ffc9e473ccd8bf5cc25528648b291b7f1b7ad02
mveron007 <mveron13814@gmail.com>
2019-06-17 14:08:27 -0300

    arreglos contacto

--- index.php
+++ index.php
@@ -49,7 +49,8 @@
 
     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
 
-    <div class="contacto">
+    <?php require_once "form.php" ?>
+    <!-- <div class="contacto">
       <h1 class="professional-pf-skill" id="contact">Contacto</h1>
       <p>Dejame tu mensaje para comunicarte conmigo</p>
 
@@ -74,10 +75,10 @@
 
       </form>
 
-    </div>
-
-    <?php require_once "footer.php" ?>
-
+    </div> -->
+    <footer>
+      <?php include "footer.php" ?>
+    </footer>
 
 
 
@@ -85,9 +86,7 @@
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-    <script src="http://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
-
-    </script>
+    <script src="http://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">   </script>
 
   </body>
 </html>
